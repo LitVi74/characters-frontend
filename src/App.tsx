1008e95e2d862b11fef1550c92b8a9b90b3983bd
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2023-04-14 15:26:04 +0500

    add userContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import {PAGES} from "./pages";
 import Header from "./components/header";
+import {IUser, UserContext} from "./hooks/useUsetContext";
+import {useState} from "react";
 
 function App() {
   const routers = useRoutes(PAGES)
 
+  const [user, setUser] = useState<IUser>();
+
   return (
-    <>
-      <Header />
+    <UserContext.Provider value={{user, setUser}}>
+      <Header isAuthorized={!!user} />
       {routers}
-    </>
+    </UserContext.Provider>
   );
 }
 
