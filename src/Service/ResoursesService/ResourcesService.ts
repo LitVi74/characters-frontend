5a60d235efb7ad414d5fa465775c21b78c1a78b6
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-04-17 16:24:35 +0300

    fix: изменить данные отправки запросов

--- src/Service/ResoursesService/ResourcesService.ts
+++ src/Service/ResoursesService/ResourcesService.ts
@@ -4,19 +4,19 @@ import api from './ResourcesAxios';
 import { SpellResponse } from './ResourcesResponse'
 
 export default class ResoursesService {
-  static async getSpells(): Promise<AxiosResponse<SpellResponse>> {
-    return api.get<SpellResponse>('/spells')
+  static async getSpells(): Promise<AxiosResponse<SpellResponse[]>> {
+    return api.get<SpellResponse[]>('/spells')
   }
 
-  static async createSpell(data): Promise<AxiosResponse<SpellResponse>> {
-    return api.post<SpellResponse>('/spells', {...data})
+  static async createSpell(data: SpellResponse): Promise<AxiosResponse<SpellResponse>> {
+    return api.post<SpellResponse>('/spells', data)
   }
 
   static async deleteSpell(spellId: String): Promise<AxiosResponse<SpellResponse>> {
     return api.delete<SpellResponse>(`/spells/${spellId}`)
   }
 
-  static async updateSpell(spellId: String): Promise<AxiosResponse<SpellResponse>> {
-    return api.update<SpellResponse>(`/spells/${spellId}`)
+  static async updateSpell(spellId: String, data: SpellResponse): Promise<AxiosResponse<SpellResponse>> {
+    return api.update<SpellResponse>(`/spells/${spellId}`, data)
   }
 }
