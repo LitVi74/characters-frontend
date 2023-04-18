7ff1b0e37dee564928ef415961a8ebbaee51271a
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-04-18 12:20:40 +0300

    refactor: импортировать функцию checkAuth в интерцептор

--- src/Service/ResoursesService/ResourcesAxios.ts
+++ src/Service/ResoursesService/ResourcesAxios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { AuthResponse } from '../AuthService/AuthResponse';
+import AuthService from '../AuthService/AuthService';
 
 const API_URL = 'http://localhost:3000';
 
@@ -21,7 +21,7 @@ api.interceptors.response.use(config => {
   if (error.response.status === 401 && error.config && !error.config._isRetry) {
     originalRequest._isRetry = true;
     try {
-      const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, { withCredentials: true });
+      const response = await AuthService.checkAuth();
       localStorage.setItem('token', response.data.accessToken);
       return api.request(originalRequest);
     } catch (e) {
