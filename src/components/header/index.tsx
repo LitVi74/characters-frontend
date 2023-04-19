ebed6dd5ed077c78ef925776ff0305550f9b270d
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-04-19 10:08:16 +0300

    feat: добавить бэкграунд кнопке расширения

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import logo from "./../../assets/ligo.svg";
+import logo from "./../../assets/logo.svg";
 import AuthorizedNavbar from "./authorized navbar";
 import UnauthorizedNavbar from "./unauthorized navbar";
 import {FC} from "react";
