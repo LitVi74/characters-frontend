107a54f49add1513a8d9eb612578d6001d34706c
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2023-05-11 21:16:35 +0500

    add: добавлены остальные фильтры
    - выделен отдельный компонент FiltersModal
    - добавлены остальные фильтры

--- src/components/SpellFilters/hooks/useFilterAction.js
+++ src/components/SpellFilters/hooks/useFilterAction.js
@@ -3,6 +3,10 @@ import {useCallback, useState} from "react";
 const useFilterAction = (filterActionList, setFilterActionList) => {
   const [selectedLevels, setSelectedLevels] = useState([]);
   const [selectedClasses, setSelectedClasses] = useState([]);
+  const [selectedSchools, setSelectedSchools] = useState([]);
+  const [selectedRitual, setSelectedRitual] = useState([]);
+  const [selectedConcentration, setsSlectedConcentration] = useState([]);
+  const [selectedCastingTime, setSelectedCastingTime] = useState([]);
 
   const deleteFilterFunctionByName = useCallback((name) => {
     setFilterActionList(filterActionList.filter(func => func.name !== name));
@@ -57,12 +61,81 @@ const useFilterAction = (filterActionList, setFilterActionList) => {
     addFilterFunctionToList(searchSpellsByClass);
   }, []);
 
+  const handleSchoolsChange = useCallback((event) => {
+    setSelectedSchools(event);
+
+    if (!event.length) {
+      deleteFilterFunctionByName("searchSpellsBySchool");
+      return;
+    }
+
+    const searchSpellsBySchool = (spell) => {
+      return spell.filter(spell => event.includes(spell.school));
+    }
+
+    addFilterFunctionToList(searchSpellsBySchool);
+  }, []);
+
+  const handleRitualChange = useCallback((event) => {
+    setSelectedRitual(event);
+
+    if (!event.length) {
+      deleteFilterFunctionByName("searchSpellsByRitual");
+      return;
+    }
+
+    const searchSpellsByRitual = (spell) => {
+      return spell.filter(spell => event.includes(spell.ritual));
+    }
+
+    addFilterFunctionToList(searchSpellsByRitual);
+  }, []);
+
+  const handleConcentrationChange = useCallback((event) => {
+    setsSlectedConcentration(event);
+
+    if (!event.length) {
+      deleteFilterFunctionByName("searchSpellsByConcentration");
+      return;
+    }
+
+    const searchSpellsByConcentration = (spell) => {
+      return spell.filter(spell => event.includes(spell.ritual));
+    }
+
+    addFilterFunctionToList(searchSpellsByConcentration);
+  }, []);
+
+  const handleCastingTimeChange = useCallback((event) => {
+    setSelectedCastingTime(event);
+
+    if (!event.length) {
+      deleteFilterFunctionByName("searchSpellsByCastingTime");
+      return;
+    }
+
+    const regExp = new RegExp(event.join('|'), 'i');
+    const searchSpellsByCastingTime = (spell) => {
+      return spell.filter(spell => regExp.test(spell.casting_time));
+    }
+
+    addFilterFunctionToList(searchSpellsByCastingTime);
+  }, []);
+
   return {
     handleSearchInputBlur,
     selectedLevels,
     handleLevelsChange,
     selectedClasses,
     handleClassesChange,
+    selectedSchools,
+    handleSchoolsChange,
+    selectedRitual,
+    handleRitualChange,
+    selectedConcentration,
+    handleConcentrationChange,
+    selectedCastingTime,
+    handleCastingTimeChange,
   };
 }
 
