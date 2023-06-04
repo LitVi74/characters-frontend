f2eef221ed27519e027ebcb11d2908b35658707e
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-06-04 16:59:38 +0300

    feat: добавить стили CharacterLink

--- src/components/CharacterLink/CharacterLink.js
+++ src/components/CharacterLink/CharacterLink.js
@@ -27,7 +27,7 @@ export default function CharacterLink({char, cbForm, cbClose}) {
   };
 
   return (
-    <li className='list-group-item' onClick={handleNavLink}>
+    <li className='list-group-item d-flex justify-content-between align-items-center' onClick={handleNavLink}>
       {name}
       <CardMenu cbForm={handleUpdate} cbDell={handleDelete} />
     </li>
