a3ea614bb9626ada15dc8dd7b4575d4d7bb23591
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-09-20 17:40:57 +0300

    feat: стилизовать страницу авторизации

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ import { CurrentUserContext } from "../../contexts/currentUserContext";
 export default function Header() {
   const { currentUser } = useContext(CurrentUserContext);
   return (
-    <header className="d-flex flex-row align-content-center w-100 px-5">
+    <header className="d-flex flex-row align-content-center w-100">
       <img alt="" src={logo} width="42" height="42" />
       {currentUser.isActivated ? <AuthorizedNavbar /> : <UnauthorizedNavbar />}
     </header>
