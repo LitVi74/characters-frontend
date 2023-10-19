a92ee1ea5b19bf03a682a44bced1469943a3c4ad
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-10-19 18:18:51 +0300

    feat: закрепить хедер в дестопном режиме

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,4 +1,7 @@
+import "./Header.scss";
 import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { PATHS } from "../../constants/constants";
 import logo from "../../../assets/logo.svg";
 
 import AuthorizedNavbar from "../AuthorizedNavbar/AuthorizedNavbar";
@@ -8,8 +11,12 @@ import { CurrentUserContext } from "../../contexts/currentUserContext";
 
 export default function Header() {
   const { currentUser } = useContext(CurrentUserContext);
+  const location = useLocation();
+  const reg = /\/characters\/[a-z0-9]+/;
+  const headerFixed = reg.test(location.pathname) ||  location.pathname === PATHS.spells;
+
   return (
-    <header className="d-flex flex-row align-content-center w-100">
+    <header className={`d-flex flex-row align-content-center w-100 ${headerFixed ? 'desctop-header' : ''}`}>
       <img alt="" src={logo} width="42" height="42" />
       {currentUser.isActivated ? <AuthorizedNavbar /> : <UnauthorizedNavbar />}
     </header>
